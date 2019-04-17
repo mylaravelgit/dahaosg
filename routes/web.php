8e8976631c475d7efd08242208d425956c7c429b
mylaravelgit <1021513892@qq.com>
2019-04-17 11:50:14 +0800

    Install Laravel

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/brand', function () {
+    return view('brand');
+});
+
+Route::get('/change', function () {
+    return view('change');
+});
+Route::get('/agency', function () {
+    return view('agency');
+});
+Route::get('/company', function () {
+    return view('company');
+});
+Route::get('/power', function () {
+    return view('power');
+});
+Route::get('/cancel', function () {
+    return view('cancel');
+});
