6a2364c719af94e1b69410e2eb918172732bd633
mylaravelgit <1021513892@qq.com>
2019-04-23 10:47:02 +0800

    sss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<!-- saved from url=(0022)http://wap.bh0531.com/ -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>大好时光</title>
+    <meta name="keywords" content="大好时光-开公司|公司注册|注册公司|代理记账">
+    <meta name="description" content="大好时光-开公司|公司注册|注册公司">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="大好时光">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="./cwzj/index.css">
+    <script language="javascript" src="http://ddt.zoosnet.net/JS/LsJS.aspx?siteid=DDT63642922&float=1&lng=cn"></script>
+</head>
+<body onclick="openZoosUrl(&#39;chatwin&#39;);">
+<div class="layout">
+    <header class="layout_header" id="layout_header">
+        <div class="action_bar">
+            <a class="list" id="toggle-btn" href="javascript:;">
+            </a>
+            <span class="app_title">
+		    	  	 </span>
+            <a href="tel:400-850-0074" class="phone">
+            </a>
+        </div>
+        <ul class="collapse" >
+            <li class="first">
+                <a onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">首页</a>
+            </li>
+            <li>
+                <a onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">联系我们</a>
+            </li>
+        </ul>
+    </header>
+    <div class="layout_body">
+        <div class="banner"  style="background:url(./foshan/background.jpg) top center no-repeat;  color:#fff; background-position: 0px 0px;">
+            <h1 style="font-size: 35px;margin-top: 50px;color: #f57a17">免费代办佛山公司注册</h1>
+            <p style="margin-top:10px; font-size:30px; text-align:center">省心/省钱的全程服务</p>
+            <h1 style="font-size: 35px;margin-top: 10px;color: #f57a17">提供佛山公司注册地址</h1>
+            <p style="margin-top:10px; font-size:30px; text-align:center">免费代办公司注册</p>
+            <p class="go_btn">
+                <a href="javascript:void(0);" onclick="openZoosUrl(&#39;chatwin&#39;);"style="">点我立即咨询</a>
+            </p>
+        </div>
+        <div class="row1">
+            <p class="title">
+                佛山公司注册
+            </p>
+            <p class="sub_title">
+                只需三步 完成注册
+            </p>
+            <div class="con">
+                <div>
+                    <img src="./cwzj/row1_i1.png">
+                    <p>免费查询</p>
+                </div>
+                <div>
+                    <img src="./cwzj/row1_i2.png">
+                    <p>人工核实</p>
+                </div>
+                <div>
+                    <img src="./cwzj/row1_i3.png">
+                    <p>极速响应</p>
+                </div>
+            </div>
+            <p>&nbsp;&nbsp;&nbsp;</p>
+            <p class="sub_title">
+                高级顾问为您把关，用户持续增长中...
+            </p>
+            <p class="title">
+                所以选择很重要
+            </p>
+            <p class="sub_title" style="font-size: 2rem; margin-top: 10px">
+                ￥ <span style="color:red">0.00</span>元佛山代理注册公司费用
+            </p>
+            <p  class="sub_title" style="color:#8e8e8e; text-decoration:line-through; font-size:14px; margin-top:14px">找其他佛山代办注册公司：￥500-1500元</p>
+            <div class="fn-clear"></div>
+            <p class="go_btn">
+                <a href="javascript:void(0);" onclick="openZoosUrl(&#39;chatwin&#39;);"style="">点我立即咨询</a>
+            </p>
+        </div>
+        <div class="row2">
+            <img src="./cwzj/row2_i1.png">
+        </div>
+{{--        <div class="row3">--}}
+{{--            <p class="title">--}}
+{{--                电话免费咨询<br>--}}
+{{--                佛山工商注册公司方案--}}
+{{--            </p>--}}
+{{--            <div class="con fn-clear">--}}
+{{--                <div class="row_to fn-clear">--}}
+{{--                    <div>--}}
+{{--                        <div class="step">1</div>--}}
+{{--                        办理时间长--}}
+{{--                    </div>--}}
+{{--                    <div>--}}
+{{--                        <div class="step">2</div>--}}
+{{--                        多跑冤枉路--}}
+{{--                    </div>--}}
+{{--                    <div>--}}
+{{--                        <div class="step">3</div>--}}
+{{--                        注册效率低--}}
+{{--                    </div>--}}
+{{--                    <div>--}}
+{{--                        <div class="step">4</div>--}}
+{{--                        多花冤枉钱--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+        <div class="row4">
+            <p class="title">
+                五大优势 金牌保障
+            </p>
+            <p class="sub_title">
+                实力说明一切
+            </p>
+            <img src="./cwzj/row4_i1.png">
+            <p class="go_btn">
+                <a href="javascript:void(0);" onclick="openZoosUrl(&#39;chatwin&#39;);"style="">点我立即咨询</a>
+            </p>
+        </div>
+
+{{--        <div class="row6">--}}
+{{--            <p class="title">--}}
+{{--                每<span class="b_c">1小时</span>就有一家公司<br>--}}
+{{--                在大好时光选择一站式注册服务--}}
+{{--            </p>--}}
+{{--            <img src="./cwzj/row6_i1.png">--}}
+{{--            <div class="con">--}}
+{{--                <div class="con_c">--}}
+{{--                    为您提供高性价比<br>--}}
+{{--                    一站式注册服务--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="row7">--}}
+{{--            <p class="title">--}}
+{{--                大好时光公司注册流程--}}
+{{--            </p>--}}
+{{--            <p class="sub_title">--}}
+{{--                一站式服务 意在满足您的各项需求--}}
+{{--            </p>--}}
+{{--            <ul>--}}
+{{--                <div class="step">--}}
+{{--                    <div class="main">--}}
+{{--                        <img src="./cwzj/row7_i1.png">--}}
+{{--                    </div>--}}
+{{--                    <div class="con">--}}
+{{--                        <span></span>--}}
+{{--                        <span></span>--}}
+{{--                        <span></span>--}}
+{{--                        <span></span>--}}
+{{--                        <span></span>--}}
+{{--                        <span></span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <li>确认合作</li>--}}
+{{--                <li>核名通知书</li>--}}
+{{--                <li>工商注册</li>--}}
+{{--                <li>刻章</li>--}}
+{{--                <li>国、地税报到</li>--}}
+{{--                <li>次月开始代账</li>--}}
+{{--                <div class="fn-clear"></div>--}}
+{{--            </ul>--}}
+{{--            <div class="fn-clear"></div>--}}
+{{--        </div>--}}
+        <div class="row8">
+            <p class="title">
+                您可能还需要的服务
+            </p>
+            <p class="sub_title">
+                一站式服务 意在满足您的各项需求
+            </p>
+            <div class="con fn-clear">
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i1.png">
+                    </p>
+                    企业代理记账
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i2.png">
+                    </p>
+                    商标注册服务
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i3.png">
+                    </p>
+                    申请一般纳税人
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i4.png">
+                    </p>
+                    汇算清缴报告
+                </div>
+            </div>
+            <p class="go_btn">
+                <a href="javascript:void(0);" onclick="openZoosUrl(&#39;chatwin&#39;);"style="">点我立即咨询</a>
+            </p>
+        </div>
+
+
+    </div>
+    <footer class="footer">
+        <div class="footer_tel">
+            <img src="./cwzj/footer_tel.png">
+        </div>
+        <div class="last_ foot_bottom_left" style="color: rgba(255, 255, 255, 0.0980392);">
+            <p><a href="/"> 佛山市禅城区雾岗路河南工业区91号三楼317 联系方式： 13825566194 </a></p>
+            <a href="http://www.beian.miit.gov.cn" >大好时光（佛山市）企业管理有限公司 粤ICP备18159108号</a>
+
+        </div>
+    </footer>
+</div>
+<script type="text/javascript" src="./cwzj/jquery-2.1.1.min.js"></script>
+
+<div id="LRdiv1" style="display:none;"></div>
+<script>
+    $(document).ready(function () {
+        $("#toggle-btn").click(function () {
+            $(".collapse").slideToggle();
+        })
+
+    })
+</script>
+
+
+</body>
+</html>
